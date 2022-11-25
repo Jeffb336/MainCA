68825ef1131953833f8c375e9fa4eea4a79d00a7
Jeffb336 <101339465+Jeffb336@users.noreply.github.com>
2022-11-25 14:39:12 +0000

    Start

--- src/mainca/MainCA.java
+++ src/mainca/MainCA.java
@@ -1,8 +1,9 @@
-/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package mainca;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,23 @@ public class MainCA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        int a,b;
+        
+        System.out.println("enter a");
+        a=sc.
+        
         // TODO code application logic here
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
